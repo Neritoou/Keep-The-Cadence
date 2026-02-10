930a6d1e206188353bfcd4a448afa640f78e83c5
odetax <odettsayegh@gmail.com>
2026-02-10 03:21:07 -0400

    feat(resources): :package: add note library to manage surfaces and particle effects

--- src/resources/loaders/__init__.py
+++ src/resources/loaders/__init__.py
@@ -2,6 +2,7 @@ from .images import _load_images
 from .fonts import _load_fonts
 from .sounds import _load_sounds
 from .spritesheets import _load_spritesheets
+from .notes import _load_notes
 from .music_paths import _load_music_paths
 
 __all__ = [
@@ -10,4 +11,5 @@ __all__ = [
     "_load_sounds",
     "_load_music_paths",
     "_load_spritesheets",
+    "_load_notes"
 ]
