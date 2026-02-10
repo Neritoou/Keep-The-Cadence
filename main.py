6117fbf20ff43b6f28b8fa41cc50900f99ccd629
odetax <odettsayegh@gmail.com>
2026-02-10 03:30:46 -0400

    refactor(core): :heavy_plus_sign: integrate enfocate core library into main game classes

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+from enfocate import GameMetadata
 from src.core.game import Game
 import pygame
 
 if not pygame.get_init():
     pygame.init()
 
-game = Game("Keep The Cadence")
+meta = GameMetadata(
+                title="Keep The Cadence",
+                description="Juego de Ritmo inspirado en Friday Night Funkin.",
+                authors=["Odett Sayegh", "Agostinho Dos Santos"],
+                group_number=4
+            )
+
+game = Game(meta)
 
 if __name__ == "__main__":
-    game.run()
+    game.run_preview()
