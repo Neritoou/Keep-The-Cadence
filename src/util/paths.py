50fcbb8781d52754b2e357b2fe9c995f61981a64
Neritoou <agdsds245@gmail.com>
2026-02-19 01:29:12 -0400

    refactor(util): :recycle: fix PyInstaller frozen executable detection in get_base_path using frozen flag instead of _MEIPASS and convert audio files from .mpeg/.mp3 to .ogg for better pygame compatibility.

--- src/util/paths.py
+++ src/util/paths.py
@@ -3,10 +3,10 @@ from pathlib import Path
 
 # Función para obtener la ruta base del proyecto dependiendo si es un ejecutable o desarrollo
 def get_base_path() -> Path:
-    base_path = getattr(sys, "_MEIPASS", None)
-    if base_path:
-        return Path(base_path)
-    
+    # PyInstaller extrae archivos en _MEIPASS
+    if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+        return Path(sys._MEIPASS)  # pyright: ignore[reportAttributeAccessIssue]
+
     # game/src/util/paths.py ->  game/
     return Path(__file__).resolve().parents[2]
 
@@ -50,19 +50,19 @@ def _get_folder_path(folder_name: str) -> Path:
     return path
 
 def get_full_song_path(folder_name: str) -> Path:
-    path = _get_folder_path(folder_name) / "full_song.mpeg"
+    path = _get_folder_path(folder_name) / "full_song.ogg"
     if not path.is_file():
         raise FileNotFoundError(f"Paths: Full Song en el folder {folder_name} no encontrado o inválido en la Ruta:\n{path}")
     return path 
 
 def get_voices_path(folder_name: str) -> Path:
-    path = _get_folder_path(folder_name) / "voices.mp3"
+    path = _get_folder_path(folder_name) / "voices.ogg"
     if not path.is_file():
         raise FileNotFoundError(f"Paths: Voices en el folder {folder_name} no encontrado o inválido en la Ruta:\n{path}")
     return path 
 
 def get_inst_path(folder_name: str) -> Path:
-    path = _get_folder_path(folder_name) / "inst.mp3"
+    path = _get_folder_path(folder_name) / "inst.ogg"
     if not path.is_file():
         raise FileNotFoundError(f"Paths: Instrumental en el folder {folder_name} no encontrado o inválido en la Ruta:\n{path}")
     return path 
