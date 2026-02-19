1d0d941e40846dc26120f808d31e12d50ee6361b
Neritoou <agdsds245@gmail.com>
2026-02-19 01:16:52 -0400

    feat(resources): :bento: add Miku spritesheet loader

--- src/resources/loaders/spritesheets.py
+++ src/resources/loaders/spritesheets.py
@@ -1,4 +1,4 @@
-from ...constants import NOTE_SIZE, NOTE_PADDING, PARTICLE_SPLASH_SIZE, PARTICLE_SPLASH_PADDING
+from ...constants import NOTE_SIZE, NOTE_PADDING, PARTICLE_SPLASH_SIZE, PARTICLE_SPLASH_PADDING, MIKU_ANIMATIONS, MIKU_ANIMATIONS_FILES
 from typing import TYPE_CHECKING
 from ...util import get_asset
 
@@ -11,3 +11,12 @@ def _load_spritesheets(rm: "ResourceManager") -> None:
 
         # TIPOS DE PARTICULAS
         rm.load_spritesheet("ParticlesType", str(get_asset("spritesheets", "particles_splash.png")), PARTICLE_SPLASH_SIZE, PARTICLE_SPLASH_PADDING)
+
+        # HATSUNE MIKU
+        for anim_name, data in MIKU_ANIMATIONS.items():
+                rm.load_spritesheet(
+                        key=f"MIKU_{anim_name}",
+                        path=str(get_asset("spritesheets", "miku", MIKU_ANIMATIONS_FILES[anim_name])),
+                        frame_size=(data["width"], data["height"]),
+                        padding=(data["padding_x"], data["padding_y"]),
+                )
