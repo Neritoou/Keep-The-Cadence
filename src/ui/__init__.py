432801a87166bb885c09f95ee2edf4a1f06d1614
Neritoou <agdsds245@gmail.com>
2026-02-26 19:47:25 -0400

    feat(ui): :sparkles: Add UICounterLabel component

--- src/ui/__init__.py
+++ src/ui/__init__.py
@@ -1,4 +1,4 @@
 from .ui_element import UIElement, ColorValue
 from .ui_manager import UIManager
 
-from .components import UIButton, UIStatic, UILabel, UIMenu, PerformanceBar, UISlideMenu, UISlideButton
+from .components import UIButton, UIStatic, UILabel, UICounterLabel, UIMenu, PerformanceBar, UISlideMenu, UISlideButton
--- src/ui/components/__init__.py
+++ src/ui/components/__init__.py
@@ -1,10 +1,11 @@
 from .static_element import UIStatic
 from .button import UIButton
 from .label import UILabel
+from .counter_label  import UICounterLabel
 from .menu import UIMenu
 from .performance_bar import PerformanceBar
 from .slide_button import UISlideButton
 from .slide_menu import UISlideMenu
 
-__all__ = ["UIStatic", "UIButton", "UILabel", "UIMenu", "PerformanceBar",
+__all__ = ["UIStatic", "UIButton", "UILabel", "UICounterLabel", "UIMenu", "PerformanceBar",
            "UISlideButton", "UISlideMenu"]
